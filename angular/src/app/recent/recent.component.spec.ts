4b40a382c43bf9002266f01482de64c91626a24e
MaximusKl <mklimkov@list.ru>
2021-09-11 23:57:59 +0300

    #Add тесты компонетов

--- angular/src/app/recent/recent.component.spec.ts
+++ angular/src/app/recent/recent.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { RecentComponent } from './recent.component'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { By } from '@angular/platform-browser'
 
 describe('RecentComponent', () => {
 	let component: RecentComponent
@@ -22,5 +23,23 @@ describe('RecentComponent', () => {
 
 	it('should create', () => {
 		expect(component).toBeTruthy()
+	})
+
+	it('should create recent content', () => {
+		const comp = fixture.debugElement.query(By.css('.recent__content'))
+
+		expect(comp.nativeElement).toBeTruthy()
+	})
+
+	it('should create input', () => {
+		const inp = fixture.debugElement.query(By.css('.recent__input-frame .recent__input input'))
+
+		expect(inp.nativeElement).toBeTruthy()
+	})
+
+	it('should create input button', () => {
+		const button = fixture.debugElement.query(By.css('.recent__input-frame button'))
+
+		expect(button.nativeElement).toBeTruthy()
 	})
 })
--- angular/src/app/tabs/tabs.component.spec.ts
+++ angular/src/app/tabs/tabs.component.spec.ts
@@ -1,25 +1,55 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { TabsComponent } from './tabs.component';
+import { TabsComponent } from './tabs.component'
+import { RouterLinkWithHref } from '@angular/router'
+import { By } from '@angular/platform-browser'
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('TabsComponent', () => {
-  let component: TabsComponent;
-  let fixture: ComponentFixture<TabsComponent>;
+	let component: TabsComponent
+	let fixture: ComponentFixture<TabsComponent>
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TabsComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [TabsComponent],
+			imports: [RouterTestingModule],
+		}).compileComponents()
+	})
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TabsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(TabsComponent)
+		component = fixture.componentInstance
+		fixture.detectChanges()
+	})
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+	it('should create', () => {
+		expect(component).toBeTruthy()
+	})
+
+	it('should have 3 routes', () => {
+		const des = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref))
+
+		expect(des.length).toBe(3)
+	})
+
+	it('should have route to [recent]', () => {
+		const des = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref))
+
+		let index = des.findIndex(el => el.properties['href'] === '/recent')
+		expect(index).toBeGreaterThan(-1)
+	})
+
+	it('should have route to [go]', () => {
+		const des = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref))
+
+		let index = des.findIndex(el => el.properties['href'] === '/go')
+		expect(index).toBeGreaterThan(-1)
+	})
+
+	it('should have route to [settings]', () => {
+		const des = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref))
+
+		let index = des.findIndex(el => el.properties['href'] === '/settings')
+		expect(index).toBeGreaterThan(-1)
+	})
+})
